147aa556c415254442c199037609b449813bb76c
Jitesh <jiteshpawar966@gmail.com>
2023-09-06 12:51:13 +0530

    add fragments

--- src/index.js
+++ src/index.js
@@ -66,16 +66,28 @@ function Header() {
 }
 
 function Menu() {
+    const pizzas = pizzaData
+    const numPizzas = pizzaData.length
     return (
         <main className="menu">
-            <h2>Our Menu</h2>
-            <ul className="pizzas">
-                {pizzaData.map((pizza) => (
-                    <Pizza 
-                        pizzaObj={pizza} key={pizza.name}
-                    />
+          <h2>Our menu</h2>
+    
+          {numPizzas > 0 ? (
+            <>
+              <p>
+                Authentic Italian cuisine. 6 creative dishes to choose from. All
+                from our stone oven, all organic, all delicious.
+              </p>
+    
+              <ul className="pizzas">
+                {pizzas.map((pizza) => (
+                  <Pizza pizzaObj={pizza} key={pizza.name} />
                 ))}
-            </ul>
+              </ul>
+            </>
+          ) : (
+            <p>We're still working on our menu. Please come back later :)</p>
+          )}
 
             {/* <Pizza 
                 name="Pizza Spinaci"
